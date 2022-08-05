8d1b413491737f12963c6a53bc493218f5c2ff24
Dmytro Stetsenko <d.a.stetsenko@gmail.com>
2022-08-05 17:19:18 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsList = document.querySelector("#ingredients");
+const list = ingredients.map((item) => {
+  const ingredient = document.createElement("li");
+  ingredient.textContent = item;
+  ingredient.classList.add("item");
+  return ingredient;
+});
+
+ingredientsList.append(...list);
+
+console.log(ingredientsList);
