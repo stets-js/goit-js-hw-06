7812fc84c2820c21c44aa7c62b50387883703219
Dmytro Stetsenko <d.a.stetsenko@gmail.com>
2022-08-05 16:31:24 +0300

    Update task-01.js

---
+++ js/task-01.js
@@ -0,0 +1,10 @@
+const categoriesCount = document.querySelectorAll(".item");
+console.log(`Number of categories: ${categoriesCount.length}`);
+
+const titleSublist = categoriesCount.forEach((elem) => {
+  console.log(
+    `Category: ${elem.firstElementChild.textContent}` +
+      `\n` +
+      `Elements: ${elem.lastElementChild.querySelectorAll("li").length}`
+  );
+});
