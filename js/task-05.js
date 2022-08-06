72a5e5d780d7092b7961e2af422e260c2d7a9995
Dmytro Stetsenko <d.a.stetsenko@gmail.com>
2022-08-06 12:29:48 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -1,4 +1,12 @@
 const input = document.querySelector("#name-input");
 const output = document.querySelector("#name-output");
 
-input.addEventListener("input", inputedText);
+input.addEventListener("input", targettingInput);
+
+function targettingInput(text) {
+  if (text.currentTarget.value === "") {
+    output.textContent = "Anonymous";
+  } else {
+    output.textContent = text.currentTarget.value;
+  }
+}
