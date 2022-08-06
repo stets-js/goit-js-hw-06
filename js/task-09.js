1583acc42c12426768d1bf0cade018a71f0dcd7c
Dmytro Stetsenko <d.a.stetsenko@gmail.com>
2022-08-06 17:53:40 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
+const changeClrButton = document.querySelector(".change-color");
+const bodyEl = document.querySelector("body");
+const colorEl = document.querySelector(".color");
+
+changeClrButton.addEventListener("click", changeClrFn);
+
+function changeClrFn() {
+  bodyEl.setAttribute("style", `background-color: ${getRandomHexColor()};`);
+  colorEl.textContent = getRandomHexColor();
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
