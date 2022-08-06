5beb78b9aa0536a8533cdee2aeaea4c9f79e02c2
Dmytro Stetsenko <d.a.stetsenko@gmail.com>
2022-08-06 16:33:25 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const formEl = document.querySelector(".login-form");
 
+formEl.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "") {
+    return alert("Всі поля повинні бути заповнені!");
+  } else if (password.value === "") {
+    return alert("Всі поля повинні бути заповнені!");
+  }
+  const userData = {
+    Email: email.value,
+    Password: password.value,
+  };
+  console.log(userData);
+  event.currentTarget.reset();
+});
